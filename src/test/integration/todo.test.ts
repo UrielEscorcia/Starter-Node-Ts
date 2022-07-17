90f079bf3883528d907a04a5be86be59a13058e6
Uriel Escorcia <uris_ec@hotmail.com>
2022-07-17 16:31:57 -0500

    fixes

--- src/test/integration/todo.test.ts
+++ src/test/integration/todo.test.ts
@@ -12,7 +12,7 @@ import {
     initializeApp,
 } from "../utils";
 
-describe("Todo testing", () => {
+describe("Todo Testing", () => {
     let request: supertest.SuperTest<supertest.Test>;
 
     beforeAll(async () => {
@@ -43,80 +43,73 @@ describe("Todo testing", () => {
     /**
      * Prompt test suite.
      */
-    describe("Todo", () => {
-        it("should get list of todos, there are 2 items", async () => {
-            const res = await request.get("/v1/todo");
-            expect(res.status).toBe(StatusCodes.OK);
-            expect(res.body).toHaveLength(2);
+
+    it("should get list of todos, there are 2 items", async () => {
+        const res = await request.get("/v1/todo");
+        expect(res.status).toBe(StatusCodes.OK);
+        expect(res.body).toHaveLength(2);
+    });
+
+    it(`should create a new todo and get it`, async () => {
+        // We define the query and the variables as you would do from your front-end
+        const body = {
+            content: `Test todo.`,
+        };
+
+        const res = await request
+            .post("/v1/todo")
+            .send(body)
+            .set("Accept", "application/json");
+
+        expect(res.status).toBe(StatusCodes.CREATED);
+        expect(res.body).toMatchObject(body);
+    });
+
+    it(`should get a todo by id`, async () => {
+        // We generate a todo ID
+        const todoId = new mongoose.Types.ObjectId().toHexString().toString();
+        // Add a todo with the generated ID in the database
+        const data = {
+            _id: todoId,
+            content: "todo 1",
+        };
+        await populateDatabase(TodoMongooseModel, [data]);
+        const res = await request.get(`/v1/todo/${todoId}`);
+        expect(res.status).toBe(StatusCodes.OK);
+        expect(res.body).toMatchObject(data);
+    });
+
+    it(`should update a todo by id and get it updated`, async () => {
+        // We generate a todo ID
+        const todoId = new mongoose.Types.ObjectId().toHexString().toString();
+        // Add a todo with the generated ID in the database
+
+        const data = {
+            _id: todoId,
+            content: "todos 1",
+        };
+        await populateDatabase(TodoMongooseModel, [data]);
+        const res = await request
+            .put(`/v1/todo/${todoId}`)
+            .send({ content: "todos 2" });
+
+        expect(res.status).toBe(StatusCodes.OK);
+        expect(res.body).toMatchObject({
+            _id: todoId,
+            content: "todos 2",
         });
+    });
 
-        it(`should create a new todo and get it`, async () => {
-            // We define the query and the variables as you would do from your front-end
-            const body = {
-                content: `Test todo.`,
-            };
-
-            const res = await request
-                .post("/v1/todo")
-                .send(body)
-                .set("Accept", "application/json");
-
-            expect(res.status).toBe(StatusCodes.CREATED);
-            expect(res.body).toMatchObject(body);
-        });
-
-        it(`should get a todo by id`, async () => {
-            // We generate a todo ID
-            const todoId = new mongoose.Types.ObjectId()
-                .toHexString()
-                .toString();
-            // Add a todo with the generated ID in the database
-            const data = {
-                _id: todoId,
-                content: "todo 1",
-            };
-            await populateDatabase(TodoMongooseModel, [data]);
-            const res = await request.get(`/v1/todo/${todoId}`);
-            expect(res.status).toBe(StatusCodes.OK);
-            expect(res.body).toMatchObject(data);
-        });
-
-        it(`should update a todo by id and get it updated`, async () => {
-            // We generate a todo ID
-            const todoId = new mongoose.Types.ObjectId()
-                .toHexString()
-                .toString();
-            // Add a todo with the generated ID in the database
-
-            const data = {
-                _id: todoId,
-                content: "todos 1",
-            };
-            await populateDatabase(TodoMongooseModel, [data]);
-            const res = await request
-                .put(`/v1/todo/${todoId}`)
-                .send({ content: "todos 2" });
-
-            expect(res.status).toBe(StatusCodes.OK);
-            expect(res.body).toMatchObject({
-                _id: todoId,
-                content: "todos 2",
-            });
-        });
-
-        it(`should delete a todo by id`, async () => {
-            // We generate a todo ID
-            const todoId = new mongoose.Types.ObjectId()
-                .toHexString()
-                .toString();
-            // Add a todo with the generated ID in the database
-            const data = {
-                _id: todoId,
-                content: "todo delete",
-            };
-            await populateDatabase(TodoMongooseModel, [data]);
-            const res = await request.delete(`/v1/todo/${todoId}`);
-            expect(res.status).toBe(StatusCodes.NO_CONTENT);
-        });
+    it(`should delete a todo by id`, async () => {
+        // We generate a todo ID
+        const todoId = new mongoose.Types.ObjectId().toHexString().toString();
+        // Add a todo with the generated ID in the database
+        const data = {
+            _id: todoId,
+            content: "todo delete",
+        };
+        await populateDatabase(TodoMongooseModel, [data]);
+        const res = await request.delete(`/v1/todo/${todoId}`);
+        expect(res.status).toBe(StatusCodes.NO_CONTENT);
     });
 });
--- src/test/utils/server.ts
+++ src/test/utils/server.ts
@@ -4,10 +4,6 @@ import expressLoader from "../../bootstrap/express";
 export const initializeApp = () => {
     const app = express();
     expressLoader(app);
-    app.post("/api/test", (req, res) => {
-        const { name } = req.body;
-        res.status(201).json({ message: "This is just a test" });
-    });
 
     return app;
 };
